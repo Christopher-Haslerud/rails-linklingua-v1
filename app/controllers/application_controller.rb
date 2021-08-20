fd9d1524e14fbfaf223da61640169404ae3a1edc
Christopher Haslerud <haslerud1@hotmail.com>
2021-08-20 18:23:46 +0200

    addding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+
+  # Forcing user to be signed in before accessing controller
+  before_action :authenticate_user!
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+
+  # allow user to access homepage without being logged on
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+  def about
   end
 end
